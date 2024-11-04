8ad5c143e9e2c6890a830c6ed91528a40402cf3b
luismede <luishenrique23hh23@gmail.com>
2024-11-04 19:59:15 -0300

    feat: using API sweapi for star wars wiki

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
-import discord, random
+import discord, os
 from discord.ext import commands
+from dotenv import load_dotenv
 
-from secret_token import token
+import requests
+
+load_dotenv()
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="$", intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
+
+blue = 0x992d22
 
 @client.event
 async def on_ready():
   print(f'Logged on as {client.user}!')
 
 @client.command()
-async def askme(ctx, arg):
-  await ctx.send(arg)
+async def hello(ctx):
+  await ctx.send(f"Hello, I'm {client.user}")
   
 
-async def result_integer(ctx, result):
-  if result.is_integer():
-    await ctx.send(f'The result is: {int(result)}')
-  else:
-    await ctx.send(f'The result is: {result:.2f}')
-    
 @client.command()
-async def math(ctx, arg1: float, command:str, arg2: float):      
-  match command:
-    case '+':
-      result = arg1 + arg2
-      await result_integer(ctx, result)
-    case 'x':
-      result = arg1 * arg2
-      await result_integer(ctx, result)
-    case '-':
-      result = arg1 - arg2
-      await result_integer(ctx, result)
-    case '/':
-      result = arg1 / arg2
-      await result_integer(ctx, result)
-    case '//':
-      result = arg1 // arg2
-      await result_integer(ctx, result)
-    case '**':
-      result = arg1 ** arg2
-      await result_integer(ctx, result)
-    case _:
-      print('This operation is invalid...')
-client.run(token())
+async def sw(ctx:commands.Context, id):
+  base_url = 'https://swapi.dev/api/people/'
+  
+  response_base = requests.get(f'{base_url + id}')
+  
+  if response_base.status_code == 404:
+    await ctx.send(f'People {id} not found. Try between 1 and 83')
+  else:
+    x = response_base.json()
+    response_home = requests.get(x['homeworld'])
+    y = response_home.json()
+    embed = discord.Embed(title='Star Wars Peoples', color=discord.Color.blue())
+    embed.add_field(name='Name: ', value=f'{x["name"]}', inline=True)
+    embed.add_field(name='Height (cm): ', value=f'{x["height"]}', inline=True)
+    embed.add_field(name='Gender: ', value=f'{x["gender"]}', inline=True)
+    embed.add_field(name='Birth Year: ', value=f'{x["birth_year"]}', inline=True)
+    embed.add_field(name='HomeWorld: ', value=f'{y["name"].capitalize()}', inline=True)
+    await ctx.send(embed=embed)
+    
+@commands.has_permissions(administrator=True)
+@client.command()
+async def clear(ctx, messages:int):
+  if messages > 50:
+    messages = 50
+    return await ctx.send("You can't clear more than 50 messages at once!")
+  await ctx.channel.purge(limit=messages + 1)
+
+client.run(os.getenv("SECRET_TOKEN"))
